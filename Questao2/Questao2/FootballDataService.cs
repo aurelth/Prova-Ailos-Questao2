8e4a750fdebd20a502ccb1621c5925dacabba3e8
Aurel Lossou <aurelth@icloud.com>
2024-11-01 18:35:41 -0300

    Implementação do Service e Program.cs

--- Questao2/Questao2/FootballDataService.cs
+++ Questao2/Questao2/FootballDataService.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Questao2
 {
     public class FootballDataService
     {
+        private readonly HttpClient _httpClient;
+
+        public FootballDataService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<int> GetGoalsByTeamAndYearAsync(string team, int year)
+        {
+            int totalGoals = 0;
+            int page = 1;
+            bool hasMorePages = true;
+
+            while (hasMorePages)
+            {
+                var url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team1={team}&page={page}";
+                var response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<ApiResponse>(content);
+
+                foreach (var match in result.Data)
+                {
+                    if (match.Team1 == team)
+                    {
+                        totalGoals += match.Team1Goals;
+                    }
+                    else if (match.Team2 == team)
+                    {
+                        totalGoals += match.Team2Goals;
+                    }
+                }
+
+                page++;
+                hasMorePages = page <= result.TotalPages;
+            }
+
+            return totalGoals;
+        }
     }
 }
--- Questao2/Questao2/Program.cs
+++ Questao2/Questao2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Questao2;
+using System;
+using System.Threading.Tasks;
+
+var service = new FootballDataService();
+
+// Exemplo de uso para calcular gols
+int psgGoals = await service.GetGoalsByTeamAndYearAsync("Paris Saint-Germain", 2013);
+Console.WriteLine($"Team Paris Saint-Germain scored {psgGoals} goals in 2013");
+
+int chelseaGoals = await service.GetGoalsByTeamAndYearAsync("Chelsea", 2014);
+Console.WriteLine($"Team Chelsea scored {chelseaGoals} goals in 2014");
