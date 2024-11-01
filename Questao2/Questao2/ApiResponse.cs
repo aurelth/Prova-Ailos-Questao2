5b2d79082b27dc119c7aafbe6db49145031413f3
Aurel Lossou <aurelth@icloud.com>
2024-11-01 18:03:38 -0300

    Criação do projeto e das principais classes

---
+++ Questao2/Questao2/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2
+{
+    public class ApiResponse
+    {
+        public int TotalPages { get; set; }
+        public List<FootballMatch> Data { get; set; }
+    }
+}
